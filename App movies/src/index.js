dc63c9b68cca0e9ed4635fc06ee4fea62657380e
Miguel Angel <96792398+MichellMurallas@users.noreply.github.com>
2022-08-19 12:56:40 -0500

    Add files via upload

---
+++ App movies/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+ReactDOM.render(
+<div>
+  <h1>Titulo</h1>
+  <div>Hola desde React</div>
+  </div>,
+  document.getElementById('root')
+  );
